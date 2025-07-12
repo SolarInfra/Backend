17eafa14a910555b4540371f613d4578b6b36774
Lavi-debug <acelavi166@gmail.com>
2025-07-12 17:11:53 +0530

    Commit 2

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ connectToMongo();
 const app = express();
 const port = 5000;
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+app.use(cors());
+
+// app.use(cors({
+//   origin: 'http://localhost:3000'
+// }));
+
 app.use(express.json());
+
 app.use('/uploads', express.static('uploads'));
 
 const storage = multer.diskStorage({
