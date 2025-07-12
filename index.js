de592b035326ec8007d7566b820cf1cbc37eee43
Lavi-debug <acelavi166@gmail.com>
2025-07-12 17:07:33 +0530

    Commit 1

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ connectToMongo();
 const app = express();
 const port = 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
